74646288d0207601723bf49975e0ad476462b1de
BCAvarad <140825143+BCAvarad@users.noreply.github.com>
2023-07-28 20:40:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,50 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import openai
 import streamlit as st
+openai.api_key="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"
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def BasicGeneration(userPrompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": userPrompt}]
+    )
+    return completion.choices[0].message.content
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.title("VS's Notes BOT-1.0")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+sub=st.text_input("Enter The Subject Name:")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+topic=st.text_input("Enter The Topic Name:")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+if st.button("Get Note's"):
+    with st.spinner("Making Note's..."):
+
+        base_prompt=f"""
+        i am BCA student and you are my mentor \
+        i will provide you name of subject.so that you can help me in that particular subject.\
+        name of subject: {sub}
+        according to subject i will provide you topic from that particular subject\
+        your task is to give information about that topic.\
+        perform the following actions:
+        1-summarize or overview the topic.
+        2-simple and easy defination of topic.
+        3-explain the working of topic in simple words.
+        4-show the syntax and give some example of topic
+        5-if need give other important information like types.if topic have types so also explain types as above format.
+        use simple tone that is easy to understand.
+        Separate your answers with line breaks.
+
+
+                topic: {topic}
+        """
+
+    output_gpt=BasicGeneration(base_prompt)
+#print(output_gpt)
+     
+
+    st.text_area("Note's", output_gpt,
+                     height=500)
+    st.success('Done!')
